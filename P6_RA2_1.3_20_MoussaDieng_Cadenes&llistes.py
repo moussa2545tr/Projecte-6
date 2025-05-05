0c87b5c7c03f2c5386bd37daf40fd39729f01463
moussa2545tr <mdieng@iesjulioantonio.cat>
2025-05-05 10:24:24 +0200

    Delete P6_RA2_1.3_20_MoussaDieng_Cadenes&llistes.py

--- P6_RA2_1.3_20_MoussaDieng_Cadenes&llistes.py
+++
@@ -1,5 +0,0 @@
-vowels = 'aeiouàèéíòóú'
-frase = input("Introdueix paraules separades per espais: ")
-paraules = frase.split()
-comencen_vocal = [p for p in paraules if p[0].lower() in vowels]
-print("Paraules que comencen per vocal:", comencen_vocal)
