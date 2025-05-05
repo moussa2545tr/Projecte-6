6e0f7283f41af53c8c7e96bb844fd41b40221e16
moussa2545tr <mdieng@iesjulioantonio.cat>
2025-05-05 10:03:29 +0200

    Delete P6_RA1_1.1_10_MoussaDieng.py

--- P6_RA1_1.1_10_MoussaDieng.py
+++
@@ -1,8 +0,0 @@
-edat1 = int(input("Introdueix la primera edat: "))
-edat2 = int(input("Introdueix la segona edat: "))
-if edat1 > edat2:
-    print("La primera persona és més gran.")
-elif edat2 > edat1:
-    print("La segona persona és més gran.")
-else:
-    print("Tenen la mateixa edat.")
