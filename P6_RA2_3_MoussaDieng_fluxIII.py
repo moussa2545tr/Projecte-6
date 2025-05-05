4917f659a88910516481fbefbed8f24492ec8cd2
moussa2545tr <mdieng@iesjulioantonio.cat>
2025-05-05 10:27:36 +0200

    Delete P6_RA2_3_MoussaDieng_fluxIII.py

--- P6_RA2_3_MoussaDieng_fluxIII.py
+++
@@ -1,7 +0,0 @@
-try:
-    n = int(input("Introdueix un nombre enter: "))
-    print(f"Taula de multiplicar del {n}:")
-    for i in range(1, 11):
-        print(f"{n} x {i} = {n * i}")
-except ValueError:
-    print("Error: has d’introduir un nombre enter.")
