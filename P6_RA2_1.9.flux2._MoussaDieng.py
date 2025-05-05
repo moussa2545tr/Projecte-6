9406d2595554fd0f04d8d8b87b9951d3a58405c8
moussa2545tr <mdieng@iesjulioantonio.cat>
2025-05-05 10:27:13 +0200

    Delete P6_RA2_1.9.flux2._MoussaDieng.py

--- P6_RA2_1.9.flux2._MoussaDieng.py
+++
@@ -1,5 +0,0 @@
-nums = list(map(float, input("Introdueix una llista de números (separats per espais): ").split()))
-if nums:
-    print("El número màxim és:", max(nums))
-else:
-    print("Llista buida.")
