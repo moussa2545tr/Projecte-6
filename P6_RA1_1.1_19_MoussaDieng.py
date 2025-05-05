6a70918f0c08781490c73d944280b2021105e2f8
moussa2545tr <mdieng@iesjulioantonio.cat>
2025-05-05 10:18:48 +0200

    Delete P6_RA1_1.1_19_MoussaDieng.py

--- P6_RA1_1.1_19_MoussaDieng.py
+++
@@ -1,4 +0,0 @@
-llargada = float(input("Introdueix la llargada de l’habitació (m): "))
-amplada = float(input("Introdueix l’amplada de l’habitació (m): "))
-superficie = llargada * amplada
-print("La superfície de l’habitació és:", superficie, "m²")
