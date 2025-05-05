9926a161355e4179189a18f6651272d7f7a924bc
moussa2545tr <mdieng@iesjulioantonio.cat>
2025-05-05 10:21:28 +0200

    Delete P6_RA2_1.1_3_MoussaDieng.py

--- P6_RA2_1.1_3_MoussaDieng.py
+++
@@ -1,6 +0,0 @@
-num1 = float(input("Introdueix el primer número: "))
-num2 = float(input("Introdueix el segon número: "))
-num3 = float(input("Introdueix el tercer número: "))
-
-maxim = max(num1, num2, num3)
-print(f"El número més gran és: {maxim}")
