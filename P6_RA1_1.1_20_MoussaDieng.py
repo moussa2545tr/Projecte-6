81e4fcba0a651cc7148a9504f363c3e2fa20f249
moussa2545tr <mdieng@iesjulioantonio.cat>
2025-05-05 10:18:56 +0200

    Delete P6_RA1_1.1_20_MoussaDieng.py

--- P6_RA1_1.1_20_MoussaDieng.py
+++
@@ -1,4 +0,0 @@
-minuts_totals = int(input("Introdueix el nombre total de minuts: "))
-hores = minuts_totals // 60
-minuts = minuts_totals % 60
-print(f"{minuts_totals} minuts són {hores} hores i {minuts} minuts.")
