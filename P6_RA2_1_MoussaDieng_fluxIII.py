52f06898161dc6d4d8a6fb7c022339e117aecdf5
moussa2545tr <mdieng@iesjulioantonio.cat>
2025-05-05 10:27:21 +0200

    Delete P6_RA2_1_MoussaDieng_fluxIII.py

--- P6_RA2_1_MoussaDieng_fluxIII.py
+++
@@ -1,6 +0,0 @@
-try:
-    n = int(input("Introdueix un número enter: "))
-    for i in range(n + 1):
-        print(i, end=" ")
-except ValueError:
-    print("Error: has d’introduir un nombre enter.")
