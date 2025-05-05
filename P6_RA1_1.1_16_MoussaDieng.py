68aea2aae6430eb44522f4b4dbd738cac204b2ad
moussa2545tr <mdieng@iesjulioantonio.cat>
2025-05-05 10:18:22 +0200

    Delete P6_RA1_1.1_16_MoussaDieng.py

--- P6_RA1_1.1_16_MoussaDieng.py
+++
@@ -1,5 +0,0 @@
-num1 = float(input("Introdueix el primer número: "))
-num2 = float(input("Introdueix el segon número: "))
-num3 = float(input("Introdueix el tercer número: "))
-mitjana = (num1 + num2 + num3) / 3
-print("La mitjana és:", mitjana)
