c558aa206457a712be460b392b8150d9a2a48e3b
moussa2545tr <mdieng@iesjulioantonio.cat>
2025-05-05 10:18:13 +0200

    Delete P6_RA1_1.1_15_MoussaDieng.py

--- P6_RA1_1.1_15_MoussaDieng.py
+++
@@ -1,4 +0,0 @@
-distancia = float(input("Introdueix la distància (km): "))
-temps = float(input("Introdueix el temps (hores): "))
-velocitat = distancia / temps
-print("La velocitat mitjana és:", velocitat, "km/h")
